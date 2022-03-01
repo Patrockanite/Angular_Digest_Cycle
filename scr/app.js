05fce2b09d474a03871b38ebfd1d0112a765f81f
Patrockanite <patrickannoot@orange.fr>
2022-03-01 22:58:31 +0100

    une partie du travail

--- scr/app.js
+++ scr/app.js
@@ -3,26 +3,24 @@
     angular.module('CounterApp',[])
         .controller('CounterController',CounterController);
 
-    CounterController.$inject = ['$scope'];
-    function CounterController($scope){
-        $scope.onceCounter =0;
+        CounterController.$inject=['$scope'];
+        function CounterController($scope){
+            $scope.onceCounter = 0;
 
+            $scope.showNumberOfWatchers = function(){
+                console.log('# of Watchers : ',$scope.$$watchersCount);
 
-        $scope.showNumberOfWatchers = function(){
-            console.log('Number(#) of watchers : ', $scope.$$watchersCount);
-        };
+            };
 
-    }
+            $scope.countOnce = function(){
+                $scope.onceCounter = 1;
+                console.log($scope.onceCounter);
 
-
-
-
-
-
-
+            };
 
 
 
+        }
 
 
 })();
